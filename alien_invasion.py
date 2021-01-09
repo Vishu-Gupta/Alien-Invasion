f1d96e4fd2b6b7c3ad5f70bdfa1b032e6d9d44c9
Vishu-Gupta <48486236+Vishu-Gupta@users.noreply.github.com>
2021-01-09 15:35:47 +0530

    Update main File
    Code made more modular and movement functionality added

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+
 class AleinInvasion:
     """Overall class to manage game assets and behaviour"""
 
@@ -10,22 +12,42 @@ class AleinInvasion:
         pygame.init()
         self.settings = Settings() # assign Settings from Settings class to self
         self.screen =pygame.display.set_mode((self.settings.
-            screen_width,self.settings.screen_height))
+                                    screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self) #create a ship attribute for this instance and pass this instance to Ship class 
 
     def run_game(self):
         #start the main loop for the game
         while True:
-            #Watch for Keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT :
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            self.screen.fill(self.settings.screen_bg_color)
 
-            #make displaly visible
+    def _check_events(self):
+        """Responds to Key presses and other events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT :
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True # move the ship right by 1 position
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP : 
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+ 
+    
+    def _update_screen(self):
+        """Updates display"""
+        self.screen.fill(self.settings.screen_bg_color)
+        self.ship.blitme()
+        self.ship.update()
+        #make display visible
+        pygame.display.flip()
 
-            pygame.display.flip()
 
 if __name__ == "__main__":
     ai = AleinInvasion()
