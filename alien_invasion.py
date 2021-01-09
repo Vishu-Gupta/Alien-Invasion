02f82b2d5aa7e9d8931ac68acaf1d652094291d2
Vishu-Gupta <48486236+Vishu-Gupta@users.noreply.github.com>
2021-01-09 16:03:16 +0530

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,8 +11,9 @@ class AleinInvasion:
         #initialize the game, and create resource
         pygame.init()
         self.settings = Settings() # assign Settings from Settings class to self
-        self.screen =pygame.display.set_mode((self.settings.
-                                    screen_width,self.settings.screen_height))
+        self.screen =pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_height = self.screen.get_rect().height
+        self.settings.screen_width = self.screen.get_rect().width
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self) #create a ship attribute for this instance and pass this instance to Ship class 
 
@@ -31,19 +32,21 @@ class AleinInvasion:
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP : 
-               self. _check_keyup_events(event)
+                self. _check_keyup_events(event)
 
  
     def _check_keydown_events(self,event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True # move the ship right by 1 position
-        if event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self,event):
-       if event.key == pygame.K_RIGHT:
+        if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
-        if event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
     def _update_screen(self):
