abadacda97a003ecf93d3e89779177b8ac520a0c
Vishu-Gupta <48486236+Vishu-Gupta@users.noreply.github.com>
2021-01-09 16:31:52 +0530

    Define Bullet class
    Inital setup for a  bullet

---
+++ bullet.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship"""
+    def __init__(self, game):
+        super().__init__() # inherit __init__
+        self.screen = game.screen
+        self.settings = game.settings
+        self.color = self.settings.bullet_color
+
+        #create a bullet at 0,0 of req size and then align with ships top.
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midtop = game.ship.rect.midtop
+
+        # Store bullets position
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """ Move the bullet up"""
+        self.y -= self.settings.bullet_speed
+        #update Bullet position to calculated location
+        self.rect.y = self.y
+        
+    def draw_bullet(self):
+        """ Draw the bullet as per settings"""
+        pygame.draw.rect(self.screen,self.color,self.rect)
